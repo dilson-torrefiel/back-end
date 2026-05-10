eacc8ad579d957924861fbd5e5b616ed0b1db199
dilson torrefiel <cpe.dilsontorrefiel@gmail.com>
2026-05-10 11:20:00 +0400

    added .gitignore

--- EJS/index.js
+++ EJS/index.js
@@ -6,11 +6,9 @@ import ejs from "ejs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const currentDate = new Date();
